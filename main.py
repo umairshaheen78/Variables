0360e036e683d11526441cf8ea0469e1384ff7b6
Umair Shaheen <109942@eesd.org>
2021-01-12 00:31:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Variables (var): containers, hold a value
+# Data Types: Variable Types:
+# String (str): combinations of characters (letters, numbers, or symbols), must use quotation marks
+# Integer (int): whole numbers
+# Characters (char): single letter, number, or symbol
+# Float: numbers with decimals
+# Boolean (bool): True/False
+# formula:
+# variableName = value
+# Rules for variable names
+# 1. always start with a letter
+# 2. can only contain numbers and letters
+# 4. if more than one word is used, use camelCase (first word is lowercase and every word after is uppercase)
+
+firstName = "Umair" #String type
+
+favoriteBook = "Series of Unfortunate Events"
+favoriteNumber = "3"
+isHungry = "True" #Boolean
+favChar = 'U' #Character
